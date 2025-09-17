66af0b9dafb027adebbdb951413ec25a85914c96
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-09-17 20:37:09 -0300

    agregar ejemplos modulos fs de tipo sync/async

---
+++ fs.sync.module.js
@@ -0,0 +1,22 @@
+import * as fs from "node:fs"
+// Escritura
+console.log("iniciando escritura");
+fs.writeFileSync('./example.txt', "<h1>Bienvenidos a NodeJS</h1>")
+console.log("finalizando escritura\n");
+
+// Lectura
+console.log("iniciando lectura");
+const data = fs.readFileSync('./example.txt');
+console.log("RAW: data", data);
+console.log("content: ", data.toString());
+console.log("finalizando lectura\n");
+
+// Edicion
+console.log("iniciando edicion");
+fs.appendFileSync('./example.txt', '\n<p>Telecom Digitalers</p>');
+console.log("finalizando edicion\n");
+
+// Eliminacion
+console.log("iniciando eliminacion");
+fs.unlinkSync('./example.txt');
+console.log("finalizando eliminacion\n");
