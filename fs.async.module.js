5608504ec5a0845b8f44f0dc2b3e38dc37957559
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-09-17 20:45:00 -0300

    integrar modulo fs a punto de entrada principal

--- fs.async.module.js
+++ fs.async.module.js
@@ -1,24 +1,29 @@
 import * as fs from "node:fs"
+
+export const fileSystemAsync = () =>{
+    console.log("\nINICIANDO PROCESO ASINCRONO")
 // Escritura
-console.log("iniciando escritura");
-fs.writeFile('./example.txt', "<h1>Bienvenidos a NodeJS</h1>", () => {
-    console.log("finalizando escritura\n");
-})
+    console.log("iniciando escritura");
+    fs.writeFile('./example.txt', "<h1>Bienvenidos a NodeJS</h1>", () => {
+        console.log("finalizando escritura\n");
+    })
 // Lectura
-console.log("iniciando lectura");
-fs.readFile('./example.txt', (err, data) => {
-    if (err) console.error("error al leer el archivo", err.code)
-    console.log("RAW: data", data);
-    console.log("content: ", data.toString());
-    console.log("finalizando lectura\n");
-});
+    console.log("iniciando lectura");
+    fs.readFile('./example.txt', (err, data) => {
+        if (err) console.error("error al leer el archivo", err.code)
+        console.log("RAW: data", data);
+        console.log("content: ", data.toString());
+        console.log("finalizando lectura\n");
+    });
 // Edicion
-console.log("iniciando edicion");
-fs.appendFile('./example.txt', '\n<p>Telecom Digitalers</p>', () => {
-    console.log("finalizando edicion\n");
-});
+    console.log("iniciando edicion");
+    fs.appendFile('./example.txt', '\n<p>Telecom Digitalers</p>', () => {
+        console.log("finalizando edicion\n");
+    });
 // Eliminacion
-console.log("iniciando eliminacion");
-fs.unlink('./example.txt', () => {
-    console.log("finalizando eliminacion\n");
-});
+    console.log("iniciando eliminacion");
+    fs.unlink('./example.txt', () => {
+        console.log("finalizando eliminacion\n");
+    });
+    console.log("FINALIZANDO PROCESO ASINCRONO\n")
+}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const info = require('./os.module')
 const url = require('./url.module.mjs')
 const path = require('./path.module.cjs')
 const { getArgs } = require('./process.module');
+const { fileSystemAsync } = require('./fs.async.module');
+const { fileSystemSync } = require('./fs.sync.module');
+
 
 const [a, b] = getArgs();
 console.log(
@@ -10,5 +13,8 @@ console.log(
     url.parseUrl(a ?? url.urlStringExample),
     "\n\nModulo Path", 
     path.parsePath(b ?? path.pathStringExample),
-    "\n\nArgumentos de Proceso"
+    "\n\nArgumentos de Proceso",
+    getArgs()
 );
+fileSystemSync();
+fileSystemAsync();
