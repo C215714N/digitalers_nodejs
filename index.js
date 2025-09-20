29a9baeffd873067152c7eddb2f1d343175c6465
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-09-19 21:50:14 -0300

    definir servidor basico con express framework

---
+++ index.js
@@ -0,0 +1,25 @@
+// Importaciones del proyecto
+import express from "express";
+// Dependencias de desarrollo
+import morgan from "morgan";
+import { configDotenv } from "dotenv";
+
+// VARIABLES DE ENTORNO: Lectura y Configuracion
+configDotenv();
+const {
+    PORT = 4200,
+    HOST = "127.0.0.1"
+} = process.env;
+const backLog = () => console.log(`servicio ejecutandose en http://${HOST}:${PORT}`)
+
+// SERVIDOR HTTP: Creacion y configuracion
+const app = express();
+app.use(morgan("dev")); // Registro de Solicitudes
+
+app.get("/home", (req, res) => {
+    res.json({
+        title: "NodeJS Digitalers",
+        message: "Bienvenido a Telecom Digitalers"
+    })
+})
+app.listen(PORT, HOST, backLog);
