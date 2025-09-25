5967cc5ec657d5bf753eea0d806c9332cd43be18
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-09-25 15:22:37 -0300

    actualizar rutas de usuarios e implementar controladores

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,18 +1,11 @@
 import { Router } from "express";
+import * as userController from "../controllers/users.controllers.js"
 
 const usersRouter = Router();
 
-usersRouter.post("/", (req, res) => 
-    res.json({message: "nuevo usuario agregado"})
-); // CREATE
-usersRouter.get('{/:id}', (req, res) => 
-    res.json({message: "usuario consultado: " + req.params.id }
-    ));  // READ
-usersRouter.put("/:id", (req, res) => 
-    res.json({message: "usuario actualizado " + req.params.id})
-);  // UPDATE
-usersRouter.delete("/:id", (req, res) => 
-    res.json({message: "usuario eliminado " + req.params.id})
-); // DELETE
+usersRouter.post("/", userController.createUser); // CREATE
+usersRouter.get('{/:id}', userController.findUserById) // READ
+usersRouter.put("/:id", userController.updateUser); // UPDATE
+usersRouter.delete("/:id", userController.deleteUser); // DELETE
 
 export default usersRouter;
