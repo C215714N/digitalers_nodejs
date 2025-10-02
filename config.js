3130dddc62a26d54a5361ce64945765ccd894192
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-10-02 01:36:57 -0300

    establecer configuracion motor de plantillas

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 export const configHandleBars = {
-
+    partialsDir: process.cwd() + "/views/partials",
+    layoutsDir: process.cwd() + "/views/layouts",
+    defaultLayout: process.cwd() + "/views/index.hbs",
+    ext: "hbs"
 };
