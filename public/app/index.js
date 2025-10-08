15e8d57731ee7114f612f6d52bf3f368ec1bd811
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-10-08 18:43:46 -0300

    configurar emision de mensajes en cliente ws

--- public/app/index.js
+++ public/app/index.js
@@ -1,3 +1,22 @@
 // Declaraciones
-
-// Eventos
+const socket = io();
+// Eventos de Navegador
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const { username, message } = e.target;
+    // verificamos el formulario
+    if (!username.value.trim() || !message.value.trim()) return;
+    // Emision del mensaje
+    socket.emit('message', {
+        username: username.value,
+        message: message.value
+    })
+    // reinicio de campo
+    username.disabled = true;
+    message.value = '';
+    message.focus();
+})
+// Eventos Conexion WebSocket
+socket.on('message', (data) => {
+    console.log(data);
+})
